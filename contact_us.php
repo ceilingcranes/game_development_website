f47581cef5ffd1dde35311400a73359ec21b42cf
Maxine <maxinehartnett@gmail.com>
2016-11-17 16:14:09 -0700

    Messing around with links, etc

---
+++ contact_us.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<div> Yello </div>
+	<?php
+	echo "testt";
+	
+	echo ", SUCK A DICK";
+	
+	?>
+	<button onclick="history.go(-1);"> Back </button>
+</body>
+</html>
